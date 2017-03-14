1a4e074d17bf36b720e9f3a25e173290b477ea38
Ian Peter Greenburg <email": null>
2017-03-14 15:31:23 -0700

    stashing some weird errors

---
+++ app/containers/searchcontainer.js
@@ -0,0 +1,31 @@
+// var React = require('react')
+// var Search = require('../components/search')
+
+// var SearchContainer = React.createClass({
+//   contextTypes: {
+//     router: React.PropTypes.object.isRequired
+//   },
+//   getInitialState() {
+//     return({
+//       search: ''
+//     })
+//   },
+//   handleSearch: function(e){
+//     e.preventDefault();
+
+//   },
+//   handleUpdateSearch: function (e) {
+//     this.setState({
+//       search: e.target.value
+//     });
+//   },
+//   render: function(){
+//     return (
+//         <Search
+//         style={this.props.style}
+//         onSearch={this.handleSearch}
+//         updateSearch={this.handleUpdateSearch}
+//         />
+//       )
+//   }
+// })
