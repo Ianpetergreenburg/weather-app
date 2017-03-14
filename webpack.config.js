183c6b9c1c07c9135af0c2dbba9a8920c03308b1
Ian Peter Greenburg <email": null>
2017-03-13 19:38:58 -0700

    more initiliazing

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.css$/, loader:'style-loader!css-loader'}
+    ]
+  },
+  plugins: [HtmlWebpackPlugConfig]
+}
