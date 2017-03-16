5e6736ba1a8e99bd893854f673f953e65f1592a9
Ian Peter Greenburg <email": null>
2017-03-15 17:10:39 -0700

    fixed bug where search bar search didn't load new props

--- app/components/header.js
+++ app/components/header.js
@@ -1,12 +1,15 @@
 var React = require('react')
 var styles = require('../styles')
 var SearchContainer = require('../containers/SearchContainer')
+var Link = require('react-router').Link
 
 var Header = React.createClass({
   render: function(){
     return(
         <div style={styles.header}>
-          <h2 style={{margin: 0}}>Clever Title</h2>
+          <Link to='/' style={styles.noLink}>
+            <h2 style={{margin: 0}}>Whatever Your Weather</h2>
+          </Link>
           <SearchContainer style={styles.searchBar}/>
         </div>
       )
